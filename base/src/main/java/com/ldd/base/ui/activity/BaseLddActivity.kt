5a2c9d33547453072803ef1a1f6e91bd19ef5927
梁迪迪 <775290428@qq.com>
2020-07-28 09:08:09 +0800

    解决使用旧的mIntent

--- base/src/main/java/com/ldd/base/ui/activity/BaseLddActivity.kt
+++ base/src/main/java/com/ldd/base/ui/activity/BaseLddActivity.kt
@@ -94,8 +94,9 @@ abstract class BaseLddActivity : AppCompatActivity() {
      */
     fun startActivityCustom(mClass: Class<*>,bundle:Bundle?){
         if(null==mIntent){
-            mIntent=Intent(mContext,mClass)
+            mIntent=Intent()
         }
+        mIntent?.setClass(mContext,mClass)
         bundle?.let { mIntent?.putExtras(it) }
         startActivity(mIntent)
     }
@@ -104,8 +105,9 @@ abstract class BaseLddActivity : AppCompatActivity() {
      */
     fun startActivityCustom(mClass: Class<*>,bundle:Bundle?,requestCode:Int){
         if(null==mIntent){
-            mIntent=Intent(mContext,mClass)
+            mIntent=Intent()
         }
+        mIntent?.setClass(mContext,mClass)
         bundle?.let { mIntent?.putExtras(it) }
         startActivityForResult(mIntent,requestCode)
     }
